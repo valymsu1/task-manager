083fac2639aca9ee6660992b4d0f1ede0abe2587
Task Manager Admin <admin@taskmanager.local>
2026-04-27 19:23:40 +0300

    Use separate PG environment variables

--- backend/db-postgres.js
+++ backend/db-postgres.js
@@ -5,7 +5,11 @@ let pool;
 const getConnection = () => {
   if (!pool) {
     pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
+      host: process.env.PGHOST || process.env.DB_HOST,
+      port: process.env.PGPORT || 5432,
+      database: process.env.PGDATABASE || process.env.DB_NAME || 'postgres',
+      user: process.env.PGUSER || process.env.DB_USER || 'postgres',
+      password: process.env.PGPASSWORD || process.env.DB_PASSWORD,
       ssl: process.env.NODE_ENV === 'production' ? {
         rejectUnauthorized: false
       } : false
